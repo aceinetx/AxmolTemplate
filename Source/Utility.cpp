6ce2cf0700633c4d1c8d0484f95583cc179cd44b
aceinetx <mouselin82@gmail.com>
2026-01-04 15:04:55 +0300

    utility upd

--- Source/Utility.cpp
+++ Source/Utility.cpp
@@ -1,10 +1,62 @@
 #include "Utility.h"
+#include "base/Director.h"
 
-auto Utility::transformOpacity(float opacity) -> uint8_t {
+namespace aceinetx::utils
+{
+auto transformOpacity(float opacity) -> uint8_t
+{
   return (uint8_t)(255.0f * opacity);
 }
 
-auto Utility::transformOpacity(uint8_t opacity) -> float {
-
+auto transformOpacity(uint8_t opacity) -> float
+{
   return (float)opacity / 255.0f;
 }
+
+auto unprojectGL(ax::Camera* camera, ax::Point p, float z) -> ax::Vec3
+{
+  auto pointNear = ax::Vec3(p.x, p.y, 0);
+  auto pointFar  = ax::Vec3(p.x, p.y, 1);
+
+  pointNear = camera->unprojectGL(pointNear);
+  pointFar  = camera->unprojectGL(pointFar);
+
+  auto x0 = pointNear.x;
+  auto y0 = pointNear.y;
+  auto z0 = pointNear.z;
+
+  auto x1 = pointFar.x;
+  auto y1 = pointFar.y;
+  auto z1 = pointFar.z;
+
+  auto x = (((z - z0) / (z1 - z0)) * (x1 - x0)) + x0;
+  auto y = (((z - z0) / (z1 - z0)) * (y1 - y0)) + y0;
+
+  return {x, y, z};
+}
+
+auto unprojectGL(ax::Camera* camera, ax::Point p) -> ax::Vec2
+{
+  ax::Vec3 pos = unprojectGL(camera, p, 0);
+  return {pos.x, pos.y};
+}
+
+void followCamera(ax::Camera* camera, ax::Node* node, ax::Vec2 diff)
+{
+  static auto* director = ax::Director::getInstance();
+  auto winSize          = director->getVisibleSize();
+
+  node->setScale(camera->getZoom());
+  node->setPosition(camera->getPosition() - winSize / 2 * camera->getZoom() + diff * camera->getZoom());
+}
+
+void followCamera(ax::Node* node, ax::Vec2 diff)
+{
+  static auto* director = ax::Director::getInstance();
+  auto camera           = director->getRunningScene()->getDefaultCamera();
+  auto winSize          = director->getVisibleSize();
+
+  node->setScale(camera->getZoom());
+  node->setPosition(camera->getPosition() - winSize / 2 * camera->getZoom() + diff * camera->getZoom());
+}
+}  // namespace aceinetx::utils
--- Source/Utility.h
+++ Source/Utility.h
@@ -1,7 +1,15 @@
 #pragma once
 #include <cstdint>
+#include "2d/Camera.h"
 
-namespace Utility {
+namespace aceinetx::utils
+{
 auto transformOpacity(uint8_t opacity) -> float;
 auto transformOpacity(float opacity) -> uint8_t;
-} // namespace Utility
+
+auto unprojectGL(ax::Camera* camera, ax::Point p, float z) -> ax::Vec3;
+auto unprojectGL(ax::Camera* camera, ax::Point p) -> ax::Vec2;
+
+void followCamera(ax::Camera* camera, ax::Node* node, ax::Vec2 diff = ax::Vec2(0, 0));
+void followCamera(ax::Node* node, ax::Vec2 diff = ax::Vec2(0, 0));
+}  // namespace aceinetx::utils
