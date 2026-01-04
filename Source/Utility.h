867720ef3c2312d09d6f9cc0178768bfd7591ad5
aceinetx <mouselin82@gmail.com>
2026-01-04 15:36:07 +0300

    improvements

--- Source/Utility.h
+++ Source/Utility.h
@@ -1,15 +1,15 @@
 #pragma once
-#include <cstdint>
 #include "2d/Camera.h"
+#include <cstdint>
 
-namespace aceinetx::utils
-{
+namespace aceinetx::utils {
 auto transformOpacity(uint8_t opacity) -> float;
 auto transformOpacity(float opacity) -> uint8_t;
 
-auto unprojectGL(ax::Camera* camera, ax::Point p, float z) -> ax::Vec3;
-auto unprojectGL(ax::Camera* camera, ax::Point p) -> ax::Vec2;
+auto unprojectGL(ax::Camera *camera, ax::Point p, float z) -> ax::Vec3;
+auto unprojectGL(ax::Camera *camera, ax::Point p) -> ax::Vec2;
 
-void followCamera(ax::Camera* camera, ax::Node* node, ax::Vec2 diff = ax::Vec2(0, 0));
-void followCamera(ax::Node* node, ax::Vec2 diff = ax::Vec2(0, 0));
-}  // namespace aceinetx::utils
+void followCamera(ax::Camera *camera, ax::Node *node,
+                  ax::Vec2 diff = ax::Vec2(0, 0));
+void followCamera(ax::Node *node, ax::Vec2 diff = ax::Vec2(0, 0));
+} // namespace aceinetx::utils
